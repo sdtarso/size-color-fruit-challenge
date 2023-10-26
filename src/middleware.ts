56401c9cb56c517a069c88ad7fd09e2f8b739ba5
Saulo Araújo <skdtarso@gmail.com>
2023-10-26 11:56:22 -0300

    Adding a example middleware

---
+++ src/middleware.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith("/home")) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+}
